63ba3c6d8b73a98e1e71997ac35f1315bfe396cc
milssky <2furius@gmail.com>
2022-01-02 21:08:47 +0300

    Незначительные доработки.

--- bitmap.py
+++ bitmap.py
@@ -1,13 +1,14 @@
 from pixel import Pixel
 from typing import Callable
 
+
 class Bitmap:
     """Класс обертка для изображения."""
     def __init__(self, pixels: list[Pixel]) -> None:
         self.pixels = pixels
 
-    def sort_index(self, sort_function: Callable) -> None:
-        """Сортирует пикселе алгоритмом из sort_function."""
+    def sort_index(self, sort_function: Callable[list[Pixel]]) -> None:
+        """Сортирует пиксели алгоритмом из sort_function."""
         pass
 
     def save_bitmap(self, name:str = "solution.bmp") -> None:
