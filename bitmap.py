15433c4c9ce0773f5d402d89c6bb4db81d7b5a4f
milssky <2furius@gmail.com>
2022-01-02 18:05:16 +0300

    Заглушки методов в Bitmap.

--- bitmap.py
+++ bitmap.py
@@ -1,9 +1,16 @@
 from pixel import Pixel
-
+from typing import Callable
 
 class Bitmap:
     """Класс обертка для изображения."""
     def __init__(self, pixels: list[Pixel]) -> None:
         self.pixels = pixels
 
-    
+    def sort_index(self, sort_function: Callable) -> None:
+        """Сортирует пикселе алгоритмом из sort_function."""
+        pass
+
+    def save_bitmap(self, name:str = "solution.bmp") -> None:
+        """Сохраняет пиксели в файл name.bmp."""
+        pass
+
