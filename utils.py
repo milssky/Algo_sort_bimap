4c4b901fd53e28041db71e1714f395d2acc5172b
milssky <2furius@gmail.com>
2022-01-03 12:32:05 +0300

    Функции считывания индексов и изображения

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from PIL import Image
+
 from pixel import Pixel
 from bitmap import Bitmap
 
@@ -5,11 +7,21 @@ from bitmap import Bitmap
 def read_image(file_path:str) -> Bitmap:
     """Вычитывает изображение из BMP файла и возвращает объект-обертку над
     этим изображением."""
-    pass
+    image = Image.open(file_path)
+    pixels:list[Pixel] = []
+    for x in range(image.width):
+        for y in range(image.height):
+            pixels.append(Pixel(x,y, image.getpixel((x,y))))
+    return Bitmap(pixels)
 
 
-def read_index_files(file_path:str) -> list[Pixel]:
+def read_index_files(file_path:str, width:int, height:int) -> list[int]:
     """Считывает из файла индексы пикселей в BMP файле."""
-    pass
+    with open(file_path) as file:
+        nums = list(map(int, file.readline().split()))
+    return nums
 
+
+def my_sort() -> None:
+    pass
 
